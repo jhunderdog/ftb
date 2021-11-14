531011fd4ae7791454c1d74ddb4f031080a8f5ac
jhunderdog <33382water@gmail.com>
2021-11-14 10:40:57 +0900

    using obx & custom class with update function

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:flutter/widgets.dart';
 import 'package:ftb/dashBoardPage.dart';
 import 'package:ftb/home_page.dart';
+import 'package:ftb/student.dart';
 import 'package:get/get.dart';
 
 void main() {
@@ -9,46 +11,57 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  var count = 0.obs;
-
-  void increment() {
-    count++;
-  }
+  // var student = Student();
+  final student = Student(name: "JH", age: 21).obs;
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-        title: " State Management Reactive ",
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text(" State Management Reactive "),
+      title: " State Management By Using Obx & Custom Class ",
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text(" State Management By Using Obx & Custom Class "),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Obx(
+                  () => Text(
+                    " Name is ${student.value.name}",
+                    style:
+                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10.0,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    // student.name.value = student.name.value.toUpperCase();
+                    student.update((student) {
+                      student!.name = student.name.toString().toUpperCase();
+                    });
+                  },
+                  child: Text(" Upper Case Letter "),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    // student.name.value = student.name.value.toLowerCase();
+                    student.update((student) {
+                      student!.name = student.name.toString().toLowerCase();
+                    });
+                  },
+                  child: Text(" Lower Case Letter"),
+                )
+              ],
             ),
-            body: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Obx(
-                    () => Text(
-                      " Count value is $count",
-                      style: TextStyle(
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 10.0,
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      increment();
-                    },
-                    child: Text(" Increment Button "),
-                  )
-                ],
-              ),
-            )));
+          )),
+    );
   }
 }
---
+++ lib/student.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+
+class Student {
+  // var name = " JH ".obs;
+  // var age = 21.obs;
+
+  var name;
+  var age;
+
+  Student({this.name, this.age});
+}
