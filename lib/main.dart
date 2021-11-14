7e2160f7fbda50582170d4ec6a760ed915c90dec
jhunderdog <33382water@gmail.com>
2021-11-14 10:55:53 +0900

    Controller

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter/widgets.dart';
+import 'package:ftb/controller.dart';
 import 'package:ftb/dashBoardPage.dart';
 import 'package:ftb/home_page.dart';
 import 'package:ftb/student.dart';
@@ -11,57 +12,53 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // var student = Student();
-  final student = Student(name: "JH", age: 21).obs;
-
-  // This widget is the root of your application.
+  MyApp({Key? key}) : super(key: key);
+  Controller controller = Get.put(Controller());
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: " State Management By Using Obx & Custom Class ",
+      title: " Reactive State Manager By Using GetX Controller ",
       home: Scaffold(
-          appBar: AppBar(
-            title: Text(" State Management By Using Obx & Custom Class "),
+        appBar: AppBar(
+          title: Text(" Reactive State Manager By Using GetX Controller "),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              GetX<Controller>(
+                // init: Controller(),
+                builder: (controller) {
+                  return Text(" The value is ${controller.count}",
+                      style: TextStyle(
+                          fontSize: 20.0, fontWeight: FontWeight.bold));
+                },
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // Get.find<Controller>().increment();
+                  controller.increment();
+                },
+                child: Text(" Increment Button "),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // Get.find<Controller>().decrement();
+                  controller.decrement();
+                },
+                child: Text(" Decrement Button "),
+              ),
+            ],
           ),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Obx(
-                  () => Text(
-                    " Name is ${student.value.name}",
-                    style:
-                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                const SizedBox(
-                  height: 10.0,
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    // student.name.value = student.name.value.toUpperCase();
-                    student.update((student) {
-                      student!.name = student.name.toString().toUpperCase();
-                    });
-                  },
-                  child: Text(" Upper Case Letter "),
-                ),
-                SizedBox(
-                  height: 10.0,
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    // student.name.value = student.name.value.toLowerCase();
-                    student.update((student) {
-                      student!.name = student.name.toString().toLowerCase();
-                    });
-                  },
-                  child: Text(" Lower Case Letter"),
-                )
-              ],
-            ),
-          )),
+        ),
+      ),
     );
   }
 }
